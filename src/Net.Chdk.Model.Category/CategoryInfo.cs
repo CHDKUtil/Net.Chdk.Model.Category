378155eecb2d788fdd3e78ff4625df19b89357d8
Dmitry Shechtman <damnian@gmail.com>
2017-06-29 05:39:06 +0300

    Add Equals() and GetHashCode() to CategoryInfo

--- src/Net.Chdk.Model.Category/CategoryInfo.cs
+++ src/Net.Chdk.Model.Category/CategoryInfo.cs
@@ -3,5 +3,16 @@
     public sealed class CategoryInfo
     {
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var categoryInfo2 = obj as CategoryInfo;
+            return Name.Equals(categoryInfo2?.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
     }
 }
